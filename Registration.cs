03098c60b020d55dbfc8f06f2ee819328b44d77a
Nitya <gorenityanand96@gmail.com>
2022-02-17 14:19:07 +0530

    [Nityanand]: UC for last name validation is added

--- Registration.cs
+++ Registration.cs
@@ -13,11 +13,24 @@ namespace UserRegistrationProgram
             string fname = Console.ReadLine();
             if (firstName.IsMatch(fname))
             {
-                System.Console.WriteLine("Your Name is :"+fname);
+                System.Console.WriteLine("Your first Name is :"+fname);
             }
             else
             {
-                System.Console.WriteLine("please enter valid name format");
+                System.Console.WriteLine("please enter valid first name!!");
+            }
+
+            Regex lastName = new Regex("^[A-Z]{1}[a-z]{3,14}$");
+
+            System.Console.WriteLine("Enter Last Name :");
+            string lname = Console.ReadLine();
+            if (lastName.IsMatch(lname))
+            {
+                System.Console.WriteLine("Your last Name is :"+lname);
+            }
+            else
+            {
+                System.Console.WriteLine("please enter valid last name!!");
             }
         }
         
